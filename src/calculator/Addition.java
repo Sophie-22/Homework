c38f2ba4ed67e58a5851523f577fadf3e0d62738
Sophie-22 <89886762+Sophie-22@users.noreply.github.com>
2021-09-12 20:59:02 -0400

    homework calculator

---
+++ src/calculator/Addition.java
@@ -0,0 +1,31 @@
+package calculator;
+import java.util.Scanner;
+
+
+public class Addition {
+
+	
+			
+public static void main (String[]args) {
+int number1 ;
+int number2;
+	
+Scanner input=new Scanner (System.in);
+
+
+System.out.println("please enter the number of adults :");
+int adults=input.nextInt();
+System.out.println("please enter the  number of kids ");
+int kids=input.nextInt();
+System.out.println(adults+kids);
+
+
+
+ input.close();
+
+ 
+
+	
+	}
+
+}
---
+++ src/calculator/Subtraction.java
@@ -0,0 +1,28 @@
+package calculator;
+import java.util.Scanner;
+
+public class Subtraction{
+public static void main(String[]args) {
+	
+Scanner input=new Scanner(System.in);
+System.out.println(" Please enter the Monthly Salary");
+int a = input.nextInt();
+System.out.println("Please enter the total of the monthly bills");
+int b = input.nextInt();
+int d= (a-b);
+System.out.println(" the subtraction  of a and b is :" +d );
+ 
+input.close();
+
+
+
+
+
+
+}
+
+
+	
+}
+
+
